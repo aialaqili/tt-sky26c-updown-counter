50fed256c365f3aaa1f8ab75ca4c6344a2366c79
aialaqili <abdullaalaqili1@gmail.com>
2026-08-10 18:33:18 +0400

    Implement 4-bit up/down counter

--- src/project.v
+++ src/project.v
@@ -1,27 +1,40 @@
 /*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
+* Copyright (c) 2026 Abdulla Alaqili
+* SPDX-License-Identifier: Apache-2.0
+*/
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_aialaqili_updown_counter (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire ena,
+    input  wire clk,
+    input  wire rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    wire direction = ui_in[0];
+    wire count_en = ui_in[1];
+    reg [3:0] count;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            count <= 4'd0;
+        end else if (count_en) begin
+            if (direction)
+                count <= count + 4'd1;
+            else
+                count <= count - 4'd1;
+        end
+    end
+
+    assign uo_out = {4'b0000, count};
+    assign uio_out = 8'b0;
+    assign uio_oe = 8'b0;
+
+    wire _unused = &{ena, ui_in[7:2], uio_in, 1'b0};
 
 endmodule
